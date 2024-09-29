be349f302765766bc775321ce066cd0e2955176c
Qusai Baker <148033591+mequsaibaker@users.noreply.github.com>
2024-09-29 15:12:46 +0200

    update app

--- obay-bakr-portfolio/src/App.jsx
+++ obay-bakr-portfolio/src/App.jsx
@@ -1,10 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import NavListAr from "./components/NavListAr/NavListAr";
 import NavBarAr from "./components/NavBar/NavBarAr";
-import DropdownList from "./components/DropdownList/DropdownList";
 
 function App() {
   const [lang, setLang] = useState("");
